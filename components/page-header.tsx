539c0956d558d18cba1f27bbd5b9cf1adb41c02f
mve <marina.veldhuizen@ivengi.com>
2023-03-15 10:12:42 +0100

    feat(card): Styling card component

--- components/page-header.tsx
+++ components/page-header.tsx
@@ -13,6 +13,15 @@ export function PageHeader({heading, text, className}: PageHeaderProps) {
             <div className="flex flex-col space-y-2">
                 <h3 className="font-title text-xl font-bold leading-tight lg:text-2xl">{heading}</h3>
             </div>
+
+
+          <div className={classNames('container px-6 py-8 mx-auto', className)}>
+            <div className="flex flex-col space-y-2 pb-4">
+              <h1 className="text-5xl font-black leading-tight">{heading}</h1>
+              {text && <p className="text-2xl font-light">{text}</p>}
+              <h3 className="text-[20px] font-title font-bold leading-tight">{heading}</h3>
+            </div>
+          </div>
         </div>
     );
 }
